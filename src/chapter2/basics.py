a5452d67413a7d4f362095926fe6eb5456ab73a5
superbeee00 <ycchoi5@gmail.com>
2025-04-12 15:37:25 +0000

    added lines of code to basic.py

--- src/chapter2/basics.py
+++ src/chapter2/basics.py
@@ -3,24 +3,24 @@
 # Below, we have a VARIABLE which is like a symbol that represents a value. You can assign a value to a VARIABLE using the '=' sign.
 # In code, we use quotes to represent text, and we call this a STRING.
 # TODO: Uncomment the line below. You can do this by removing the `#`. Next, assign your name to the `name` VARIABLE, enclosed in quotes. 
-# name = 
+name = "Jordan Choi"
 
 # TODO: Pass in the `name` VARIABLE to the `print` FUNCTION to print your name. We can pass VARIABLES as ARGUMENTS to FUNCTIONS.
-# print(PUT_NAME_HERE)
+print(name)
 
 # TODO: In your TERMINAL, run the command `python src/chapter2/basics.py` You should see your name on the TERMINAL window.
 # Notice how we give the path of the `basics.py` file to run. If in your TERMINAL you are running from a different location, we would need to provide a different path.
 
 # TODO: Assign your favorite number to the `favorite_number` VARIABLE. This time, you don't need to use quotes because numbers are not text.
-# favorite_number = 
+favorite_number = 1
 
 # TODO: Assign another number to the `another_number` VARIABLE.
-# another_number = 
+another_number = 99
 
 # TODO Uncomment the line below.
-# print(favorite_number + another_number)
+print(favorite_number + another_number)
 
 # TODO: In your TERMINAL, run the command `python src/chapter2/basics.py`. You should now also see the sum of your favorite number and another number.
 
 # TODO BONUS: How would you print the result of favorite_number * another_number?
-# print(YOUR_ANSWER_HERE)
+print(favorite_number * another_number)
